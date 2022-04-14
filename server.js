b6cdcdf3648708d0054a619f220ed14a5e3205a1
SamFarFar <sfarzamfar@yahoo.com>
2022-04-14 17:39:48 -0600

    server files have been Fully Created

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const CourseAPI = require('./API/course');
+const StudentAPI = require('./API/student');
+const ScheduleAPI = require('./API/schedule');
+const AdminAPI = require('./API/admin');
 
 
 app.use('/course',CourseAPI);
+app.use('/student',StudentAPI);
+app.use('/schedule',ScheduleAPI);
+app.use('/admin',AdminAPI);
+
 // localhost:5000/course/courses
 
 app.listen(port, () => {

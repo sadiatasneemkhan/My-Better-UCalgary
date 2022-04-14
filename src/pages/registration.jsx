d7ae7db0abbe587ae0df94b5a35571323e8eef3c
Sadia Khan <81999006+sadiatasneemkhan@users.noreply.github.com>
2022-04-14 17:22:37 -0600

    registration changed from class to function

--- src/pages/registration.jsx
+++ src/pages/registration.jsx
@@ -2,33 +2,31 @@ import React, { Component } from 'react';
 import LoginHeader2 from '../components/loginHeader2';
 import '../styles/registration.css'
 
-class Registration extends Component {
-    render() { 
-        return (
-            <React.Fragment> 
-                <LoginHeader2/>
-                <h1>REGISTER AS</h1>
-                <div className="btn">
-                
-                <button
-                    type="button"
-                    className="button margin-right student"
-                    onClick={event =>  window.location.href='../pages/studentRegistration.jsx'}
-                >
-                    <span className="buttontext">STUDENT</span>
-                </button>
-                
-                <button
-                    type="button"
-                    className="button admin"
-                    onClick={event =>  window.location.href='../pages/adminRegistration.jsx'}
-                >
-                    <span className="buttontext">ADMIN</span>
-                </button>
-                </div>
-            </React.Fragment>
-        );
-    }
+export default function Registration (props) {
+   
+    return (
+        <React.Fragment> 
+            <LoginHeader2/>
+            <h1>REGISTER AS</h1>
+            <div className="btn">
+            
+            <button
+                type="button"
+                className="button margin-right student"
+                onClick={event =>  window.location.href='../pages/studentRegistration.jsx'}
+            >
+                <span className="buttontext">STUDENT</span>
+            </button>
+            
+            <button
+                type="button"
+                className="button admin"
+                onClick={event =>  window.location.href='../pages/adminRegistration.jsx'}
+            >
+                <span className="buttontext">ADMIN</span>
+            </button>
+            </div>
+        </React.Fragment>
+    ); 
 }
  
-export default Registration;

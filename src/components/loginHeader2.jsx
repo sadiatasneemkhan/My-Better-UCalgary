daeb45a4e708fd3d2d676c7fba3f6b6a338ef206
Sadia Khan <81999006+sadiatasneemkhan@users.noreply.github.com>
2022-04-14 17:17:54 -0600

    login header 2 changed form class to function comp

--- src/components/loginHeader2.jsx
+++ src/components/loginHeader2.jsx
@@ -1,26 +1,21 @@
 import React, {Component} from 'react';
 import '../styles/loginHeader.css'
 
-class LoginHeader2 extends Component {
-
-    render() { 
-        return (
-            <React.Fragment>
-                <div className="header">
-                <div className="login">
-                    <button
-                    className="log-in-button"
-                    onClick={event =>  window.location.href='../pages/login.jsx'}
-                    >
-                    LOGIN
-                    </button>
-                </div>
+export default function LoginHeader2 (props) { 
+    return (
+        <React.Fragment>
+            <div className="header">
+            <div className="login">
+                <button
+                className="log-in-button"
+                onClick={event =>  window.location.href='../pages/login.jsx'}
+                >
+                LOGIN
+                </button>
             </div>
-            <img src="../images/logo.png" class="unilogo" />
-            </React.Fragment>
-            
-        );
-    }
+        </div>
+        <img src="../images/logo.png" class="unilogo" />
+        </React.Fragment>
+        
+    );
 }
- 
-export default LoginHeader2;

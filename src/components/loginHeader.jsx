14e4db840fe48cf2e7e627a780b35ad55ce19631
Sadia Khan <81999006+sadiatasneemkhan@users.noreply.github.com>
2022-04-14 17:19:57 -0600

    login header 1 changed from class to fucntion

--- src/components/loginHeader.jsx
+++ src/components/loginHeader.jsx
@@ -1,22 +1,17 @@
 import React, {Component} from 'react';
 import '../styles/loginHeader.css'
 
-class LoginHeader extends Component {
-
-    render() { 
-        return (
-            <div className="header">
-                <div className="login">
-                    <button
-                    className="log-in-button"
-                    onClick={event =>  window.location.href='../pages/login.jsx'}
-                    >
-                    LOGIN
-                    </button>
-                </div>
+export default function LoginHeader (props) {
+    return (
+        <div className="header">
+            <div className="login">
+                <button
+                className="log-in-button"
+                onClick={event =>  window.location.href='../pages/login.jsx'}
+                >
+                LOGIN
+                </button>
             </div>
-        );
-    }
+        </div>
+    );
 }
- 
-export default LoginHeader;

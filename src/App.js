5bd5905d4122adfe1ecf9d1d093115735043bf08
Sadia Khan <81999006+sadiatasneemkhan@users.noreply.github.com>
2022-04-15 17:15:57 -0600

    page not found made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import PageNotFound from './pages/pageNotFound';
 import Homepage from './pages/homepage';
 import Login from './pages/login';
+import Registration from './pages/registration';
 
 export default function App (){
 
@@ -11,7 +13,18 @@ export default function App (){
       <Switch>
         <Route path="/" exact component={Homepage}/>
         <Route path="/login" exact component={Login}/>
-        
+        <Route path="/" exact component={Registration}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/" exact component={Homepage}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/" exact component={Homepage}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/" exact component={Homepage}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/" exact component={Homepage}/>
+        <Route path="/login" exact component={Login}/>
+
+        <Route path="*" exact component={PageNotFound}/>
       </Switch>
 
 

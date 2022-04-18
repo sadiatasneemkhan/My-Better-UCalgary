aa7a0cfc4dfe605b17f07910827fcb2da1cb36f1
Sadia Khan <81999006+sadiatasneemkhan@users.noreply.github.com>
2022-04-18 17:25:33 -0600

    confirm pass works for admin ref

--- src/pages/adminRegistration.jsx
+++ src/pages/adminRegistration.jsx
@@ -1,50 +1,22 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, useRef } from "react";
 import LoginHeader2 from '../components/loginHeader2';
-import axios from "axios";
 import ar from '../styles/adminRegistration.module.css'
+import axios from "axios";
+import {  useParams } from "react-router-dom";
 
-// makes it so that the API is not called the moment the page is rendered
-const useDidMountEffect = (fetcher) => {
-
-    const didMount = useRef(false);
-
-    useEffect(() => {
-        if(didMount.current)
-      fetcher();
-      else{
-          didMount.current = true;
-      }
-    }, []);
-  };
-
-  //http://localhost:5001/admin/createadmin?First_name=${fname}&Last_name=${lname}&UCID=${user}&pass=${pass}&date=${date}
+const qs = require('qs');
 
 export default function AdminRegistration (props) {
-    
+
     const [fname, setFName] = useState("");
     const [lname, setLName] = useState("");
-    const [user, setUser] = useState("");
-    const [pass, setPass] = useState("");
-    const [date, setDate] = useState("");
-
-    async function fetcher() {
-        const url = `http://localhost:5001/student/checklogin?UCID=${user}&password=${pass}`;
-        const { data } = await axios.get(url);
-        console.log(data);
-        if (data.account == "ERROR") {
-        alert("The username or Password entered was not correct");
-        } else if (data.account == "Admin") {
-        document.location.href = `${window.location.origin}/admin/?ucid=${user}`;
-        } else if (data.account == "Student") {
-        document.location.href = `${window.location.origin}/student/?ucid=${user}`;
-        }
-    }
-
-    const submission = (evt) => {
-        evt.preventDefault();
-        fetcher();
-    };
-
+    const [ucid, setUCID] = useState("");
+    const [password, setPass] = useState("")
+    const [confPassword, setConfPass] = useState("");
+    const [deptName, setDeptName] = useState("");
+    const [deptHead, setDeptHead] = useState("");
+    const [dateJoined, setDate] = useState("");
+    
     const useDidMountEffect = (fetcher) => {
 
         const didMount = useRef(false);
@@ -58,40 +30,138 @@ export default function AdminRegistration (props) {
         }, []);
     };
 
+    async function fetcher(){
+    console.log("ran fetcher")
+
+    //formatting of date to remove / from input
+    let formattedDate =
+    dateJoined.substring(0, 2) + dateJoined.substring(3, 5) + dateJoined.substring(6);
+  
+    let post = {
+        First_name: fname,
+        Last_name: lname,
+        UCID: ucid,
+        pass: password,
+        Confirmed_pass: confPassword,
+        Dept_name: deptName,
+        Dept_head: deptHead,
+        date: formattedDate
+    };
+
+    var bool = Boolean(
+        post.First_name.length < 255 && 
+        post.Last_name.length < 255 && 
+        post.UCID.length == 8 && 
+        post.pass.length < 255 && 
+        post.Confirmed_pass.length < 255 && 
+        post.Dept_name.length < 255 && 
+        post.Dept_head.length < 255 &&
+
+        // date has NO / --> ex. 01/01/2001 is 01012001
+        post.date.length == 8
+    ); 
+
+        const url = `http://localhost:5001/admin/createadmin`;
+        if(bool){
+        const {data} = await axios.post(url, null, 
+        {
+            params: post
+        });
+        
+        if(data !== null && (post.pass === post.Confirmed_pass)){
+            document.location.href = `${window.location.origin}/admin`;
+            alert(`${ucid}: Admin account has been successfully created.`)
+        }else{
+            alert(`An error has orrcurred while creating the account for: ${ucid}`);
+        }
+    }else{
+        alert(`There was an error in your inputs.`);
+    }
+    }
+
+    const submission = (evt) => {
+        evt.preventDefault();
+        fetcher();
+    }
+
     return (
         <React.Fragment> 
             <LoginHeader2/>
             <div className={ar.regbox}>
                 <h1>REGISTRATION</h1>
 
-                <form method="post">
+                <form onSubmit={submission}>
                     <div className={ar.labels}>
                         <label>Full Name</label>
                     </div>
                     <div className={ar.textfield}>
-                        <input className={ar.center_block} type="text" placeholder="First Name" required />
-                        <input className={ar.center_block} type="text" placeholder="Last Name" required />
+                        <input 
+                            className={ar.center_block} 
+                            type="text" 
+                            onChange={(e) => setFName(e.target.value)}
+                            placeholder="First Name" 
+                            required />
+                        <input 
+                            className={ar.center_block} 
+                            type="text" 
+                            onChange={(e) => setLName(e.target.value)}
+                            placeholder="Last Name" 
+                            required />
                     </div>
 
                     <div className={ar.labels}>
                         <label>Date Joined</label>
                     </div>
                     <div className={ar.textfield}>
-                    <input className={ar.center_block} type="text" placeholder="DD/MM/YYYY" required />
+                        <input 
+                            className={ar.center_block} 
+                            type="text" 
+                            onChange={(e) => setDate(e.target.value)}
+                            placeholder="DD/MM/YYYY" 
+                            required />
                     </div>
 
                     <div className={ar.labels}>
                         <label>Account Info</label>
                     </div>
                     <div className={ar.textfield}>
-                    <input className={ar.center_block} type="text" placeholder="UCID" required />
-                    <input className={ar.center_block} type="text" placeholder="Department" required />
-                    <input className={ar.center_block} type="text" placeholder="Password" required />
-                    <input className={ar.center_block} type="text" placeholder="Confirm Password" required />
+                        <input 
+                            className={ar.center_block} 
+                            type="text" 
+                            onChange={(e) => setUCID(e.target.value)}
+                            placeholder="UCID" 
+                            required />
+                        <input 
+                            className={ar.center_block} 
+                            type="text" 
+                            onChange={(e) => setDeptName(e.target.value)}
+                            placeholder="Department Name" 
+                            required />
+                        <input 
+                            className={ar.center_block} 
+                            type="text" 
+                            onChange={(e) => setDeptHead(e.target.value)}
+                            placeholder="Dept Head's Name" 
+                            required />
+                        <input 
+                            className={ar.center_block} 
+                            type="password" 
+                            onChange={(e) => setPass(e.target.value)}
+                            placeholder="Password" 
+                            required />
+                        <input 
+                            className={ar.center_block} 
+                            type="password" 
+                            onChange={(e) => setConfPass(e.target.value)}
+                            placeholder="Confirm Password" 
+                            required />
                     </div>
 
                     <div className={ar.button}>
-                        <input className={ar.center_block} type="submit" value="Register" />
+                        <input 
+                            className={ar.center_block} 
+                            type="submit" 
+                            value="Register" />
                     </div>
                     
                 </form>

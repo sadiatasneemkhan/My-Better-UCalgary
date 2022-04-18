a0abbedbe1b8734697a815d9d235c12988b683e5
Sadia Khan <81999006+sadiatasneemkhan@users.noreply.github.com>
2022-04-17 21:13:33 -0600

    plain login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,45 +4,20 @@ import axios from 'axios';
 import l from '../styles/login.module.css'
 
 export default function Login (props) {
-
-    const [user, setUser] = useState("");
-    const [pass, setPass] = useState("");
-    const [hasLoaded,setHasLoaded] = useState([{
-        loading:false,
-        account: null,
-    }]);
-
-    const submission = (evt) => {
-        evt.preventDefault();
-        alert(`user: ${user} pass: ${pass}`);
-        const url = `http://localhost:5001/student/checklogin?UCID=${user}&password=${pass}`;
-
-    }
-    
-    useEffect(() => {
-        const url = `http://localhost:5001/student/checklogin?UCID=${user}&password=${pass}`;
-        axios.get(url).then((account) =>{
-            const acc = account.data;
-            console.log();
-    });
-
-}, [])
-    
     return (
         <React.Fragment> 
             <LoginHeader2/>
             <div className={l.loginbox}>
                 <h1>LOGIN</h1>
                 
-                <form onSubmit={submission}>
+                <form method="post">
                     <div className={l.textfield}>
-                    <input className={l.center_block} onChange={e => setUser(e.target.value)}type="text" placeholder="UCID" required />
+                    <input className={l.center_block} type="text" placeholder="UCID" required />
                     </div>
 
                     <div className={l.textfield}>
                     <input
                         className={l.center_block}
-                        onChange={e => setPass(e.target.value)} 
                         type="password"
                         placeholder="Password"
                         required

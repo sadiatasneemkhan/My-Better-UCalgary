bc49258a302d3753a98d67671114cf7183176cb0
Sadia Khan <81999006+sadiatasneemkhan@users.noreply.github.com>
2022-04-19 23:01:16 -0600

    add grade fucnitonaliy implemented

--- src/pages/addGrade.jsx
+++ src/pages/addGrade.jsx
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import AdminDashboard from '../components/adminDashboard';
 import ag from '../styles/addGrade.module.css'
+import axios from "axios";
 
 function AddGrade() {
+    let ucid;
+    let i = new URLSearchParams(window.location.href.split("?")[1]);
+    i.forEach((para) => {
+        ucid = para;
+    });
+
+    const [sUCID, setUCID] = useState("");
+    const [courseName, setCourseName] = useState("");
+    const [sem, setSem] = useState("");
+    const [pgrade, setPGrade] = useState("");
+    const [lgrade, setLGrade] = useState("");
+
+    const useDidMountEffect = (fetcher) => {
+        const didMount = useRef(false);
+    
+        useEffect(() => {
+          if (didMount.current) fetcher();
+          else {
+            didMount.current = true;
+          }
+        }, []);
+    };
+
+    async function fetcher() {
+        console.log("ran fetcher");
+    
+        let post = {
+            Adminid: ucid,
+            Sid: sUCID,
+            Course_name: courseName,
+            Sem: sem,
+            Lgrade: lgrade,
+            Pgrade: pgrade,
+        };
+
+        var bool = Boolean(
+            post.Adminid.length == 8 &&
+            post.Sem.length <= 11 &&
+            post.Sid.length == 8 &&
+            post.Course_name.length == 9 &&
+            post.Lgrade.length <= 2 &&
+            post.Pgrade.length <= 2 
+        );
+
+        const url = `http://localhost:5001/admin/addGrade`;
+        if (bool) {
+          const { data } = await axios.post(url, null, {
+            params: post,
+          });
+    
+          //await axios.post(url,qs.stringify(post));
+          if (data !== null) {
+            document.location.href = `${window.location.origin}/admin/?ucid=${ucid}`;
+            alert(`${courseName} grade has been added for ${sUCID}.`);
+          } else {
+            alert(`An error has orrcurred while adding grade for: ${sUCID}`);
+          }
+        } else {
+          alert(`There was a error in your inputs.`);
+        }
+      }
+    
+      const submission = (evt) => {
+        evt.preventDefault();
+        fetcher();
+      };
+
     return (  
         <React.Fragment>
             <AdminDashboard/>
@@ -10,12 +78,17 @@ function AddGrade() {
             <div className={ag.grade_details}>
             <h1>ADD GRADE</h1>
 
-            <form method="post">
+            <form onSubmit={submission}>
                 <div className={ag.labels}>
                 <label>Student's UCID</label>
                 </div>
                 <div className={ag.textfield}>
-                <input className={ag.center_block} type="text" placeholder="UCID" required />
+                <input 
+                    className={ag.center_block} 
+                    type="text" 
+                    onChange={(e) => setUCID(e.target.value)}
+                    placeholder="UCID" 
+                    required />
                 </div>
                 <div className={ag.labels}>
                 <label>Course Name</label>
@@ -24,6 +97,7 @@ function AddGrade() {
                 <input
                     className={ag.center_block}
                     type="text"
+                    onChange={(e) => setCourseName(e.target.value)}
                     placeholder="Ex. CPSC 471"
                     required
                 />
@@ -36,6 +110,7 @@ function AddGrade() {
                 <input
                     className={ag.center_block}
                     type="text"
+                    onChange={(e) => setSem(e.target.value)}
                     placeholder="Ex. Winter 2022"
                     required
                 />
@@ -48,6 +123,7 @@ function AddGrade() {
                 <input
                     className={ag.center_block}
                     type="text"
+                    onChange={(e) => setPGrade(e.target.value)}
                     placeholder="Ex. 97"
                     required
                 />
@@ -60,6 +136,7 @@ function AddGrade() {
                 <input
                     className={ag.center_block}
                     type="text"
+                    onChange={(e) => setLGrade(e.target.value)}
                     placeholder="Ex. A+"
                     required
                 />
